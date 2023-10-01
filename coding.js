274ccefc33bbcc7b4ec2f09855343a24cd387d65
^NKOJO^ <^nkojo3529@gmail.com^>
2023-10-01 01:04:41 +0000

    changes

---
+++ coding.js
@@ -0,0 +1,61 @@
+// string
+var hisName = "name here";
+let myName = "Matthew";
+// float/decima
+const pi = 3.142;
+// number/ int
+let age = 34;
+// Arrays
+let data = ["Matthew", 3.142, 34, ["solo", 232, true]];
+// objects
+const person = {
+  name: "Matthew",
+  age: 10,
+  address: "Coastal Road",
+  about: "A Tech King",
+  scores: [12, 54, 54, 654],
+  contact: {
+    phone: 12344343,
+    email: "addaematthew14@gmail.com",
+  },
+  isMarried: false,
+};
+// Booleans
+// true and false
+
+// ##################
+//  Operators
+//#################
+
+// String contcatenations
+let statement =
+  "My name is " + person.name + " and i am " + person.age + " years old ";
+//better way using string literals/backticks ` `
+let statement2 = `My name is ${person.name} and i am ${person.age} years old`;
+// Addition
+
+let x = 10;
+let y = 4;
+let sum = x + y;
+
+// try substraction,multiplication and division on your own
+// using dot notation to get the values of object person
+// console.log(person.about);
+// using scare notation to get the values of object person
+// console.log(person["name"]);
+// using index/scare notation to get the values of datas array
+// console.log(data[2]);
+
+// console.log(statement);
+// console.log(statement2);
+let is_cloudy = false
+let temperature = 100
+if(){
+
+}
+// else if(){
+
+// }
+else(){
+
+}
